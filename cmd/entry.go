3387cbccd00c6d211b3da2b94154cf5b922a44d5
Elliot Brookes <elliotbrookes1@gmail.com>
2023-11-18 11:36:04 +0000

    added in a router init function, and small scaffolding in entry point

--- cmd/entry.go
+++ cmd/entry.go
@@ -1,8 +1,20 @@
 package cmd
 
-import "fmt"
+import (
+	"ElliotBrookes/halo-platform/internal/startup"
+	"fmt"
+)
 
 func main() {
 
 	fmt.Println("Starting Server...")
+	// get a configured server
+	s, err := startup.InitServer()
+	if err != nil {
+		return
+	}
+
+	// Init and configure the routers for the application routes as well as adapters.
+
+	s.ListenAndServe()
 }
--- internal/startup/router.go
+++ internal/startup/router.go
@@ -1,1 +1,6 @@
 package startup
+
+func RegisterRoutes() {
+	// Register all routers in the ports dir
+
+}
